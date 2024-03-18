f30225f19a3c631fbe809781e033d0f45d24771a
Eshwar <eshwar.devaramane@yellow.ai>
2024-03-18 23:45:55 +0530

    fix:API base change

--- frontend/src/services/api.service.js
+++ frontend/src/services/api.service.js
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3001";
+const BASE_URL = import.meta.env.VITE_SERVER_BASE_URL;
 const apiClient = axios.create({
   baseURL: BASE_URL,
   headers: {
